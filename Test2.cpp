0616e240d957e30a3881d64f49481a31d876d992
[jayesh-685] <jayeshkhurana685@gmail.com>
2021-01-09 11:01:17 +0530

    Menu using function

--- Test2.cpp
+++ Test2.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <string>
-#include <ctime>
+#include <iomanip>
 #include <vector>
 using namespace std;
 
+double a_penny_doubled_everyday(int n, double p = 0.01);
 
 int main()
 {
-    vector <int> numbers {1, 2, 3, 4};
-    cout << numbers[0] << endl;
+    double total_amount = a_penny_doubled_everyday(25);
+
+        cout <<  "If I start with a penny and doubled it every day for 25 days, I will have $" << setprecision(10) << total_amount;
 
     return 0;
+}
+
+double a_penny_doubled_everyday(int n, double p) 
+{
+    if(n == 1)
+        return p;
+    return 2 * a_penny_doubled_everyday(n-1, p);
 }
